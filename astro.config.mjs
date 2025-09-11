7bf385a0c2dc3bdd905a2fc72ccb7884ea01bcbe
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2025-09-11 18:08:55 +0900

    初期設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,29 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
+        resolve: {
+            alias: {
+                "@": "/src",
+                "@assets": "/src/assets",
+                "@img": "/src/assets/img",
+                "@styles": "/src/assets/styles",
+                "@utils": "/src/assets/utils",
+                "@components": "/src/components",
+                "@layouts": "/src/layouts",
+            },
+        },
+    },
+
+    server: {
+        host: true,
+    },
 });
